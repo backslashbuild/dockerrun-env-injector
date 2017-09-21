a24268d43288cd9fbf8ef9046518d0c91325f7e5
Steven Borrie <saborrie@gmail.com>
2017-09-21 15:54:14 +0100

    fixed relative import stuff

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const writeFile = util.promisify(fs.writeFile);
 
 const handleError = error => {
   console.error(error);
-  process.exit(0);
+  process.exit(1);
 }
 
 program
@@ -22,20 +22,26 @@ program
   .option('-e, --env <env>', 'The dotenv file, defaults to ./.env')
   .action(async containerName => {
 
-    const template = program.template && path.resolve(program.template) || "./Dockerrun.aws.template.json";
-    const output = program.output && path.resolve(program.output) || "./Dockerrun.aws.json";
-    const env = program.env && path.resolve(program.env) || "./.env";
+    if(!containerName) {
+      return handleError('Must provide container name');
+    }
 
-    console.log(`using template ${template}, output ${output}, env ${env}`)
+    try {
+      const template = program.template && path.resolve(program.template) || path.resolve('Dockerrun.aws.template.json');
+      const output = program.output && path.resolve(program.output) || path.resolve('Dockerrun.aws.json');
+      const env = program.env && path.resolve(program.env) || path.resolve('.env');
 
-    const config = dotenv.parse(await readFile(env, { encoding: 'utf8' }).catch(handleError));
-    const dockerrun = require(template);
+      console.log(`using template ${template}, output ${output}, env ${env}`)
 
-    console.log(dockerrun);
+      const config = dotenv.parse(await readFile(env, { encoding: 'utf8' }).catch(handleError));
+      const dockerrun = require(template);
 
-    const dockerrunEnvironment = dockerrun.containerDefinitions.find(container => container.name === containerName).environment;
-    Object.keys(config).forEach(name => dockerrunEnvironment.push({ name, value: config[name] }));
-    await writeFile(output, JSON.stringify(dockerrun, null, 2)).catch(handleError);
+      const dockerrunEnvironment = dockerrun.containerDefinitions.find(container => container.name === containerName).environment;
+      Object.keys(config).forEach(name => dockerrunEnvironment.push({ name, value: config[name] }));
+      await writeFile(output, JSON.stringify(dockerrun, null, 2)).catch(handleError);
+    } catch (e) {
+      return handleError(e);
+    }
   });
 
 program.parse(process.argv);
